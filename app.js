1fc38f12eb129d23a3857482b71c68b0f5f3d01b
Rassas Amine <rassas.ahmed.amine@gmail.com>
2017-06-13 06:01:05 +0200

    All source files

---
+++ app.js
@@ -0,0 +1,13 @@
+// just like working on c :D
+function navActive(x){
+  $(".active").removeClass("active");
+  if (x==p1) {
+    $("#p1").addClass("active");
+  }
+  else if(x==p2){
+    $("#p2").addClass("active");
+  }
+  else if (x==p3) {
+    $("#p3").addClass("active");
+  }
+};
